450505c64cfcdd5492a3b9f40daec76266cf84df
hedi926 <66388098+hedi926@users.noreply.github.com>
2024-10-05 11:58:47 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,14 @@
 <?php
 $host = 'localhost';
 $db = 'test';
-$user = 'root'; // Remplacez par votre nom d'utilisateur
-$pass = ''; // Remplacez par votre mot de passe
+$user = 'hedi'; 
+$pass = '1234'; 
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass); // cette ligne sert à faire une connexion avec la base de données
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Could not connect to the database: " . $e->getMessage());
+    die("Could not connect to the database: " . $e->getMessage()); // cette ligne sert à catch une erreur et s'il y'en a une affichier le message "Could not..."
 }
 ?>
